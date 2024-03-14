469ca0cc7922c296ff19296fe41322f7e135bf17
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-14 18:40:28 +0330

    feat: customize tailwind class

--- src/Features/Authentication/SendOPTForm.jsx
+++ src/Features/Authentication/SendOPTForm.jsx
@@ -7,9 +7,9 @@ const SendOPTForm = () => {
         <form className='w-full space-y-8'>
             <div >
                 <label htmlFor='PhoneNumber' className='mb-1'>شماره موبایل</label>
-                <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} id='PhoneNumber' placeholder='شماره موبایل' type='text' className='text-secondary-900 hover:border-primary-300 focus:border-primary-300 transition-colors focus:shadow-lg focus:shadow-primary-200' />
+                <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} id='PhoneNumber' placeholder='شماره موبایل' type='text' className='textField-input' />
             </div>
-            <button className='w-full my-4 bg-primary-900 hover:bg-primary-800 shadow-lg shadow-primary-300 text-white font-DanaBold py-2 rounded-lg transition-colors'>ارسال کد تایید</button>
+            <button className='btn btn-primary w-full'>ارسال کد تایید</button>
         </form>
     </div>
   )
