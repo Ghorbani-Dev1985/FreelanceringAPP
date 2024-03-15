cecf51b1207c86ed565006dd44e1a8f9b0b6a01b
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-15 20:26:40 +0330

    feat: handle access and refresh token in HttpService file

--- src/Services/HttpService.js
+++ src/Services/HttpService.js
@@ -1,9 +1,33 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:5000/api"
+
 const Api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: BASE_URL,
     withCredentials: true
 })
+
+Api.interceptors.request.use(
+    res => res,
+    err => Promise.reject(err)
+)
+
+Api.interceptors.response.use(
+    res => res,
+   async (err) => {
+      const originalConfig = err.config;
+        if(err.response.status === 401 && !originalConfig._retry){
+            originalConfig._retry = true;
+            try {
+              const {data} = await axios.get(`${BASE_URL}/user/refresh-token` , {withCredentials: true})
+              if(data) return Api(originalConfig)
+            } catch (error) {
+                return Promise.reject(error)
+            }
+        }
+        Promise.reject(err)
+    }
+)
 
 const Http = {
     get: Api.get,
