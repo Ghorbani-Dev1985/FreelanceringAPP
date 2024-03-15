63bfa343b429da35e10b39e1aadff812e994ad12
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-15 20:33:07 +0330

    change not found image

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import NotFoundImg from './../../Assets/Images/NotFound/NotFound.svg'
+import NotFoundImg from './../../Assets/Images/NotFound/NotFound.png'
 
 const NotFound = () => {
   return (
     <section className='flex-center mt-14'>
     <div className='container sm:mt-0'>
-      <div className='flex-center mb-3'><img src={NotFoundImg} className='w-full max-w-4xl' alt='ghorbani-dev.ir' /></div>
+      <div className='flex-center mb-3'><img src={NotFoundImg} className='w-full max-w-3xl' alt='ghorbani-dev.ir' /></div>
         <h3 className="font-DanaBold text-xl sm:text-3xl text-zinc-700 dark:text-white text-center sm:-mt-[60px]">صفحه ای که دنبالش بودی پیدا نشد </h3>
         <div className='flex-center mt-5 sm:mt-10'>
         <Link to='/' className="flex-center button-md mb-8 rounded-2xl sm:button-lg btn-primary mx-auto hover:text-white">بازگشت به صفحه اصلی</Link>
