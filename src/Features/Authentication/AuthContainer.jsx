9819ef867e27d1e0ca3269b1a3488d097a8d7c7a
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-16 12:51:49 +0330

    feat: develop AuthLayout and add in CompleteProfileForm component

--- src/Features/Authentication/AuthContainer.jsx
+++ src/Features/Authentication/AuthContainer.jsx
@@ -5,7 +5,6 @@ import CenterLogin from './../../Assets/Images/Login/centerLogin.png'
 import toast from 'react-hot-toast'
 import { useMutation } from '@tanstack/react-query'
 import { GetOtp } from '../../Services/AuthService'
-import CompleteProfileForm from './CompleteProfileForm'
 
 
 const AuthContainer = () => {
@@ -37,23 +36,33 @@ const AuthContainer = () => {
     }
   return (
     <>
-    <div className='h-full w-full lg:w-[30%] lg:bg-gray-100 bg-gray-100 flex-center lg:justify-end'>
-        <div className='w-full max-w-sm lg:ml-[-200px] z-20 relative'><div className='bg-white rounded-2xl p-8 w-full'>{RenderStep()}</div></div>
-    </div>
-        <div className='hidden lg:block h-full w-[70%] rounded-tr-4xl rounded-br-4xl z-0 bg-gradient-to-tr from-primary-900 to-primary-800'>
-            <div className='flex-center h-full w-full'>
-                <div className='max-w-lg flex-center flex-col'>
-                    <img src={CenterLogin} alt='ghorbani-dev.ir' className='size-96' />
-                    <h3 className='font-MorabbaBold text-white text-3xl my-6'>پروژه مورد نظرتو پیدا کن</h3>
-                    <p className='text-white text-justify mt-8'>
-                        به راحتی می تونی کارفرما های مختلف با پروژه های متفاوتی که تعریف می کنن رو پیدا کنی و بهشون پیشنهاد بدی و وارد مذاکره بشی تا 
-                        بتونی پروژه بگیری و انجامش بدی و بیکار نمونی و هرجا نیاز به مشاوره داشتی ما هستیم
-                    </p>
-                </div>
-            </div>
-        </div>
+    <AuthLayout imgSrc={CenterLogin} title="پروژه مورد نظرتو پیدا کن" subTitle="به راحتی می تونی کارفرما های مختلف با پروژه های متفاوتی که تعریف می کنن رو پیدا کنی و بهشون پیشنهاد بدی و وارد مذاکره بشی تا 
+                        بتونی پروژه بگیری و انجامش بدی و بیکار نمونی و هرجا نیاز به مشاوره داشتی ما هستیم"> {RenderStep()}</AuthLayout>
     </>
   )
 }
 
 export default AuthContainer
+
+const AuthLayout = ({children , imgSrc , title , subTitle}) => {
+    return(
+        <>
+        <div className='h-full w-full lg:w-[30%] lg:bg-gray-100 bg-gray-100 flex-center lg:justify-end'>
+            <div className='w-full max-w-sm lg:ml-[-200px] z-20 relative'><div className='bg-white rounded-2xl p-8 w-full'>{children}</div></div>
+        </div>
+            <div className='hidden lg:block h-full w-[70%] rounded-tr-4xl rounded-br-4xl z-0 bg-gradient-to-tr from-primary-900 to-primary-800'>
+                <div className='flex-center h-full w-full'>
+                    <div className='max-w-lg flex-center flex-col'>
+                        <img src={imgSrc} alt='ghorbani-dev.ir' className='size-96' />
+                        <h3 className='font-MorabbaBold text-white text-3xl my-6'>{title}</h3>
+                        <p className='text-white text-justify mt-8'>
+                           {subTitle}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export {AuthLayout}
--- src/Features/Authentication/CompleteProfileForm.jsx
+++ src/Features/Authentication/CompleteProfileForm.jsx
@@ -6,6 +6,8 @@ import { CompleteProfile } from '../../Services/AuthService'
 import toast from 'react-hot-toast'
 import Loading from '../../UI/Loading'
 import { useNavigate } from 'react-router-dom'
+import {AuthLayout} from "./AuthContainer"
+import CompleteProfileImg from "./../../Assets/Images/CompleteProfile/completeProfile.png"
 
 const CompleteProfileForm = () => {
     const [fullName , setFullName] = useState("")
@@ -34,8 +36,9 @@ const CompleteProfileForm = () => {
         }
     }
   return (
-    <section className='flex-center h-screen'>
-    <div className='w-full sm:max-w-sm bg-white rounded-lg p-6'>  
+    <>
+    <section className='flex h-screen'>
+    <AuthLayout imgSrc={CompleteProfileImg} title="پروفایل خودتو تکمیل کن" subTitle="این کار و که انجام بدی مشخص میشه که کارفرما هستی یا فریلنسر تا بتونی از قابلیت های اپ کاملا استفاده کنی پس همین الان فرم رو تکمیلش کن">
         <div className='flex-center my-4'><h2 className='font-MorabbaBold text-2xl'>تکمیل اطلاعات کاربری</h2></div>   
         <form onSubmit={CompleteProfileHandler} className='space-y-8'>
         <TextField label=" نام و نام خانوادگی" name="FullName" value={fullName} onChange={(e) => setFullName(e.target.value)} placeholder=" نام و نام خانوادگی خود را وارد نمایید"/>
@@ -45,12 +48,13 @@ const CompleteProfileForm = () => {
                 <RadioInput label="فریلنسر" name="role" value="FREELANCER" onChange={(e) => setRole(e.target.value)} checked={role === "FREELANCER"}/>
         </div>
         {
-            isPending ? <Loading /> :
-        <button type='submit' className='btn btn-primary w-full'>ثبت اطلاعات</button>
+          isPending ? <Loading /> :
+          <button type='submit' className='btn btn-primary w-full'>ثبت اطلاعات</button>
         }
         </form>
-    </div>
-    </section>
+    </AuthLayout>
+        </section>
+      </>
   )
 }
 
