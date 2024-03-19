27810a73192e2056a3721d47bd11cb2682c0d3be
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-19 22:54:28 +0330

    feat: develop protected route for roles

--- src/UI/ProtectedRoute.jsx
+++ src/UI/ProtectedRoute.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useAuthorize from '../Features/Authentication/useAuthorize'
+import { useNavigate } from 'react-router-dom'
+import Loading from './Loading'
 
 const ProtectedRoute = ({children}) => {
+    const navigate = useNavigate()
   const {isAuthenticated , isAuthorized , isLoading} = useAuthorize()
-  return children
+  useEffect(() => {
+    if(!isAuthenticated && !isLoading) navigate('/auth' , {replace: true})
+    if(!isAuthorized && !isLoading) navigate('/notAccess' , {replace: true})
+  }, [isAuthenticated , isAuthorized , isLoading , navigate])
+if(isLoading) return <div className='flex-center h-screen bg-gray-100'><Loading /></div>
+ if(isAuthenticated && isAuthorized) return children
 }
 
 export default ProtectedRoute
