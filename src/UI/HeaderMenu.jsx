4489ea4178c66409f1f8437b119ad102d3d7f39e
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-21 19:17:21 +0330

    feat: delete dasgboard link in header3

--- src/UI/HeaderMenu.jsx
+++ src/UI/HeaderMenu.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { HiOutlineUser } from "react-icons/hi2";
 import DarkModeToggle from "./DarkModeToggle";
 import Logout from "../Features/Authentication/Logout";
 
 const HeaderMenu = () => {
   return (
     <ul className="flex-center gap-x-2">
-      <li>
-        <Link to="dashboard">
-          <HiOutlineUser className="size-6 text-primary-900" />
-        </Link>
-      </li>
       <li>
        <DarkModeToggle />
       </li>
