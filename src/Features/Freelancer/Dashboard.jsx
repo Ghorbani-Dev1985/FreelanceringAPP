57941fedb6f14f55fd70d267df2f79f09a8bc9d3
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-23 19:20:24 +0330

    feat: develop chart in freelancer dashboard

--- src/Features/Freelancer/Dashboard.jsx
+++ src/Features/Freelancer/Dashboard.jsx
@@ -3,6 +3,7 @@ import DashboardHeader from '../../UI/DashboardHeader'
 import Stats from './Stats'
 import Loading from '../../UI/Loading'
 import useProposals from '../Proposals/useProposals'
+import Chart from '../../UI/Chart'
 
 const Dashboard = () => {
   const {isLoading , proposals} = useProposals()
@@ -11,6 +12,7 @@ const Dashboard = () => {
     <>
     <DashboardHeader title="گزارش عملکرد" subTitle="تو این نمای کلی یک گزارش کلی از درخواست های خودت و همچنین میزان درآمدت که به کیف پولت اضافه شده رو مشاهده می کنی" />
     <Stats proposals={proposals} />
+    <Chart title=" درخواست‌های ثبت شده" key={proposals._id} data={proposals} dataKey="price" XdataKey="duration" />
     </>
   )
 }
